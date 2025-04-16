a238a51a988827294d13b940d6446e49a0578e78
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-04-16 17:09:45 +0600

    profile done

--- lib/widgets/customDrawerWidget.dart
+++ lib/widgets/customDrawerWidget.dart
@@ -1,11 +1,40 @@
 import 'package:AppStore/utils/AppColor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
+import '../helper/profile_page_database_helper.dart';
 import '../pages/User_Profile_Page.dart';
 
-class Customdrawerwidget extends StatelessWidget {
+class Customdrawerwidget extends StatefulWidget {
   const Customdrawerwidget({super.key});
+
+  @override
+  State<Customdrawerwidget> createState() => _CustomdrawerwidgetState();
+}
+
+class _CustomdrawerwidgetState extends State<Customdrawerwidget> {
+  String userName = 'Loading...';
+  String userEmail = 'Loading...';
+
+  @override
+  void initState() {
+    super.initState();
+    loadProfileData();
+  }
+
+  Future<void> loadProfileData() async {
+    final profile = await DatabaseHelper.instance.getUserProfile();
+    if (profile != null) {
+      setState(() {
+        userName = profile['name'] ?? 'No Name';
+        userEmail = profile['email'] ?? 'No Email';
+      });
+    } else {
+      setState(() {
+        userName = 'No Name Found';
+        userEmail = 'No Email Found';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,35 +42,45 @@ class Customdrawerwidget extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-              colors: [AppColor.pink1,AppColor.pink2,AppColor.pink3,AppColor.pink1],
-              tileMode: TileMode.repeated,
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft
-          )
+            colors: [AppColor.pink1, AppColor.pink2, AppColor.pink3, AppColor.pink1],
+            tileMode: TileMode.repeated,
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          ),
         ),
         child: ListView(
           children: [
             DrawerHeader(
-                child: Row(
-                  children: [
-                    Icon(Icons.account_circle,color: AppColor.yellowAccent,size: 78.sp,),
-                    SizedBox(width: 10.w,),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text("Profile Name",style: TextStyle(color: AppColor.white,fontSize: 20.sp,fontWeight: FontWeight.bold),),
-                          SizedBox(height: 2.h,),
-                          SingleChildScrollView(
-                              scrollDirection: Axis.horizontal,
-                              child: Text("example123@gmail.com",style: TextStyle(color: AppColor.white,fontSize: 15.sp,),),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                )
+              child: Row(
+                children: [
+                  Icon(Icons.account_circle, color: AppColor.yellowAccent, size: 78.sp),
+                  SizedBox(width: 10.w),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          userName,
+                          style: TextStyle(
+                            color: AppColor.white,
+                            fontSize: 20.sp,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 2.h),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Text(
+                            userEmail,
+                            style: TextStyle(color: AppColor.white, fontSize: 15.sp),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
             _buildDrawerItem(Icons.edit, 'Edit Profile', context, EditProfilePage()),
             _buildDrawerItem(Icons.layers, 'My Orders', context, EditProfilePage()),
@@ -62,13 +101,22 @@ class Customdrawerwidget extends StatelessWidget {
     );
   }
 
-  Widget _buildDrawerItem(IconData icon, String title, BuildContext context, Widget destinationPage){
+  Widget _buildDrawerItem(
+      IconData icon, String title, BuildContext context, Widget destinationPage) {
     return ListTile(
-      leading: Icon(icon,color: AppColor.yellowAccent,),
-      title: Text(title,style: TextStyle(color: AppColor.yellowAccent, fontSize: 16.sp),),
-      onTap: () {
+      leading: Icon(icon, color: AppColor.yellowAccent),
+      title: Text(
+        title,
+        style: TextStyle(color: AppColor.yellowAccent, fontSize: 16.sp),
+      ),
+      onTap: () async {
         Navigator.pop(context);
-        Navigator.push(context, MaterialPageRoute(builder: (context) => destinationPage,));
+        final result = await Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => destinationPage),
+        );
+        // Optional: Drawer এ ফেরত আসলে আবার লোড করাও
+        await loadProfileData();
       },
     );
   }
